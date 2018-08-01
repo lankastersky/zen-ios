8686a34687214129854e009e059539289a3d02a3
Anton Popov <lankastersky@gmail.com>
2018-08-01 13:56:12 -0700

    unused code in MutableChallengeData for coding/encoding removed

--- zen/models/MutableChallengeData.swift
+++ zen/models/MutableChallengeData.swift
@@ -9,14 +9,6 @@ final class MutableChallengeData: Codable {
     let rating: Double?
     let comments: String?
 
-//    enum CodingKeys: String, CodingKey {
-//        case challengeId
-//        case status
-//        case finishedTime
-//        case rating
-//        case comments
-//    }
-
     init(
         _ challengeId: String,
         _ status: ChallengeStatus?,
@@ -30,22 +22,4 @@ final class MutableChallengeData: Codable {
         self.rating = rating
         self.comments = comments
     }
-
-//    public init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//
-//        challengeId = try container.decode(String.self, forKey: .challengeId)
-//        if let statusInt = try container.decodeIfPresent(Int.self, forKey: .status) {
-//            status = ChallengeStatus(rawValue: statusInt)
-//        } else {
-//            status = nil
-//        }
-//        finishedTime = try container.decode(Double.self, forKey: .finishedTime)
-//        rating = try container.decodeIfPresent(Double.self, forKey: .rating)
-//        comments = try container.decodeIfPresent(String.self, forKey: .comments)
-//    }
-//
-//    public func encode(to encoder: Encoder) throws {
-//        
-//    }
 }
